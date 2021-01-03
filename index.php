3377922aa6d34701960d7238a28cc8e7ea20dc34
bwakedavis <bwakedavis@gmail.com>
2021-01-03 07:51:46 +0300

    Changed the index file to php to enable the file system writing

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crypto Portfolio Price Tracker HTML Widget builder</title>
+</head>
+<body>
+    <p>Fields - Name,Quantity, Purchase Price, Price, Cost, Market Value, Return, %Return</p>
+    <p>Add Row for the coin bought at</p>
+    <p>Rows per page</p>
+    <p>Search</p>
+    <p>Sort Field</p>
+    <p>Sort Direction</p>
+    <p>Color</p>
+    <p>Extra CSS TABLE STLES</p>
+    <script>
+        const baseUrl = "https://api.coingecko.com/api/v3"
+    </script>
+</body>
+</html>

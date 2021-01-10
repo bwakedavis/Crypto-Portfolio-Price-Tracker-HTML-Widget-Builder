0fcbd4b1dd8e60a3cd2f796a5c299bcc4e02d7ca
bwakedavis <bwakedavis@gmail.com>
2021-01-10 11:47:07 +0300

    fetch price from the coingecko api and display the in the dom succesfully

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const baseUrl = "https://api.coingecko.com/api/v3/";
     const PriceAndMarketCap = "https://api.coingecko.com/api/v3/https://api.coingecko.com/api/v3/";
     async function getCoinList(){
-        let response = await fetch(`${baseUrl}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6000&page=1&sparkline=false`);
+        let response = await fetch(`${baseUrl}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=60&page=1&sparkline=false`);
         let data = await response.json();
         return data;
     };
@@ -21,7 +21,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
             nameTd.classList.add('.name-td');
             let quantityTd = document.createElement('td');
             let purchasePriceTd = document.createElement('td');
-            let marketPriceTd = document.createElement('td');
+            let PriceTd = document.createElement('td');
             let costTd = document.createElement('td');
             let marketValueTd = document.createElement('td');
             let returnTd = document.createElement('td');
@@ -34,12 +34,39 @@ window.addEventListener('DOMContentLoaded', (event) => {
             let SpanTextTr = document.createElement('span');
             SpanTextTr.textContent = coin.name;
 
+            //handle the quantity field
+            let quantityInput = document.createElement('input')
+            quantityInput.type = "text";
+            quantityInput.className = "quantity-input";
+
+            //handle purchase price field
+            let purchasePriceInput = document.createElement('input');
+            purchasePriceInput.type = "text";
+            purchasePriceInput.className = "purchase-price-input";
+
+            //handle current price
+            PriceTd.textContent = coin.current_price;
+
+
+
+
             //append data into the DOM
+            //append the name field
             nameTd.appendChild(coinImage);
             nameTd.appendChild(SpanTextTr);
             firstRd.appendChild(nameTd);
             firstRow.appendChild(firstRd);
-            
+            //append the quantity field
+            quantityTd.appendChild(quantityInput);
+            firstRd.appendChild(quantityTd);
+            firstRow.appendChild(firstRd);
+            //append the purchase price field
+            purchasePriceTd.appendChild(purchasePriceInput);
+            firstRd.appendChild(purchasePriceTd);
+            firstRow.appendChild(firstRd);
+            //append current price
+            firstRd.appendChild(PriceTd);
+            firstRow.appendChild(firstRd);
         })
     }).catch(err => console.log(err))
     
